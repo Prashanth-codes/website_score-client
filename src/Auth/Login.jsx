f829395bd70b8d21a70ebdb3950cf0b08722527b
Prashanth Nelluri <147067694+Prashanth-codes@users.noreply.github.com>
2024-12-10 17:00:25 +0530

    Update Login.jsx

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
       setErrorMessage('');
 
       try {
-        const response = await axios.post('http://localhost:8000/login', { email, password });
+        const response = await axios.post('https://website-score-server.onrender.com/login', { email, password });
         localStorage.setItem('token', response.data.accessToken);
         navigate('/score');
       } catch (error) {
